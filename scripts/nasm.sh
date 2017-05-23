782a127828c5aab0f1974d701c75105da2f77ab5
George Stoyanov <gstoyanov@gmail.com>
2017-05-23 11:36:39 +0000

    changed the nasm git repo

--- scripts/nasm.sh
+++ scripts/nasm.sh
@@ -2,7 +2,7 @@
 
 mkdir $HOME/install
 cd $HOME/install
-git clone https://github.com/letolabs/nasm.git
+git clone https://github.com/cyrillos/nasm.git 
 cd $HOME/install/nasm
 sh autogen.sh
 sh configure
