f9a9823a32ad50a24e530fd2d303cac0510a5f58
George Stoyanov <gstoyanov@gmail.com>
2017-06-02 14:09:30 +0200

    removed --enable-freetype, libass, libschroedinger

--- scripts/ffmpeg.sh
+++ scripts/ffmpeg.sh
@@ -5,9 +5,9 @@ cd $HOME/install
 git clone https://github.com/FFmpeg/FFmpeg.git
 cd $HOME/install/FFmpeg
 ./configure \
---enable-gpl --enable-ladspa --enable-libpulse --enable-libschroedinger --enable-libsoxr \
+--enable-gpl --enable-ladspa --enable-libpulse --enable-libsoxr \
 --enable-libspeex --enable-avfilter --enable-avresample --enable-postproc --enable-pthreads \
---enable-libass --enable-libfdk-aac --enable-libfreetype --enable-libmp3lame --enable-libopus \
+--enable-libfdk-aac --enable-libmp3lame --enable-libopus \
 --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libx264 --enable-libx265 \
 --enable-nonfree --enable-libkvazaar --disable-ffplay --enable-libopenjpeg --enable-libwebp \
 --enable-libxvid --enable-libvidstab --enable-libopenh264 --enable-zlib --enable-openssl
