763899ff11b97f0ad862598fb9899e4e818ec5f8
George Stoyanov <gstoyanov@gmail.com>
2018-05-14 11:31:06 +0200

    Update libmp3lame.sh

--- scripts/libmp3lame.sh
+++ scripts/libmp3lame.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#mkdir $HOME/install
-cd $HOME/install
+cd $HOME/ffmpeg/install
 git clone https://github.com/gypified/libmp3lame.git
-cd $HOME/install/libmp3lame
-./configure --enable-static --enable-nasm --disable-shared 
+cd libmp3lame
+PATH="$HOME/ffmpeg/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg/lib/pkgconfig"
+./configure --enable-static --prefix="$HOME/ffmpeg/install" --bindir="$HOME/ffmpeg/bin" --enable-pic --disable-shared --enable-nasm
 make
 sudo make install
 make distclean
+sudo ldconfig
