032c063f1be9d5337f6cc90cd89060b9e0e020b8
George Stoyanov <gstoyanov@gmail.com>
2018-05-14 11:27:42 +0200

    modified  couple of lines

--- scripts/kvazaar.sh
+++ scripts/kvazaar.sh
@@ -5,7 +5,8 @@ cd $HOME/install
 git clone https://github.com/ultravideo/kvazaar.git
 cd $HOME/install/kvazaar
 ./autogen.sh
-./configure --enable-static
+PATH="$HOME/ffmpeg/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg/lib/pkgconfig"
+./configure --enable-static --prefix="$HOME/ffmpeg/install" --bindir="$HOME/ffmpeg/bin" --enable-pic
 make
 sudo make install
 make distclean
