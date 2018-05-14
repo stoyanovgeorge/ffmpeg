a484ea9841769fc54abb7060b4ebe09e38b6b67f
George Stoyanov <gstoyanov@gmail.com>
2018-05-14 11:26:10 +0200

    added PATH, PKG_CONFIG_PATH, prefix and bindir

--- scripts/dirac.sh
+++ scripts/dirac.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
-#mkdir $HOME/install
-cd $HOME/install
+cd $HOME/ffmpeg/install
 git clone https://github.com/DIRACGrid/DIRAC.git
-cd $HOME/install/DIRAC
-./configure --enable-static
+cd DIRAC
+PATH="$HOME/ffmpeg/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg/lib/pkgconfig"
+./configure --enable-static --prefix="$HOME/ffmpeg/install" --bindir="$HOME/ffmpeg/bin" --enable-pic
 make
 sudo make install
 make distclean
