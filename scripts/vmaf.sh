842dc22ccddefc0d1bf19d5c9fcb0412479e0563
stoyanovgeorge <gstoyanov@gmail.com>
2018-05-28 10:27:55 +0000

    fixed the functions in vmaf.sh script

--- scripts/vmaf.sh
+++ scripts/vmaf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Installing the prerequisite packages
-function prerequisites {
+prerequisites() {
         sudo apt update -qq
         sudo apt install -y \
                 pkg-config \
@@ -14,30 +14,30 @@ function prerequisites {
                 python-dev \
                 python-pip \
                 python-tk \
-                git &> /dev/null
+                git 1> /dev/null
         sudo -H pip install --upgrade pip 1> /dev/null
         pip install --user numpy scipy matplotlib notebook pandas sympy nose scikit-learn scikit-image h5py 1> /dev/null
-        echo ""export PATH="$PATH:$HOME/.local/bin""" >> $HOME/.bashrc 1> /dev/null
-        source "$HOME"/.bashrc 1> /dev/null
+        echo ""export PATH="$PATH:$HOME/.local/bin""" >> $HOME/.bashrc
+        source "$HOME"/.bashrc
 }
 
 # Cloning the VMAF repository from Github
-function vmaf_compilation {
-	cd $HOME/ffmpeg/downloads
+vmaf_compilation() {
+        cd "$HOME"/ffmpeg/downloads
         git clone https://github.com/Netflix/vmaf.git 1> /dev/null
         cd vmaf
-        ./configure 1> /dev/null
-        make -j"$cpu_cnt" 1> /dev/null
-        sudo make install 1> /dev/null
+        ./configure
+        make 1> /dev/null
+        sudo make install >1 /dev/null
 }
 
 # Testing the VMAF installation
-function vmaf_testing {
+vmaf_testing() {
         git submodule update --init --recursive 1> /dev/null
         make -j"$cpu_cnt" 1> /dev/null
         echo ""export PYTHONPATH="$(pwd)/python/src:$(pwd)/sureal/python/src:$PYTHONPATH""" >> $HOME/.bashrc
         source "$HOME"/.bashrc
-        ./unittest 
+        ./unittest
 }
 prerequisites
 vmaf_compilation
