166ca3dab856b859fa44dd7c5b762f5f60818118
George Stoyanov <gstoyanov@gmail.com>
2020-02-26 11:32:29 +0100

    updated the download link
    VideoLan has switched the repo location from Github to Gitlab.

--- scripts/libx264.sh
+++ scripts/libx264.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd $HOME/ffmpeg/downloads
-git -C x264 pull 2> /dev/null || git clone --depth 1 https://git.videolan.org/git/x264
+git -C x264 pull 2> /dev/null || git clone --depth 1 https://code.videolan.org/videolan/x264.git
 cd x264 
 ./configure --enable-static --enable-pic
 make -j$cpu_cnt
